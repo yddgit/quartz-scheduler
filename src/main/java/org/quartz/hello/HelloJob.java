20ad5634725646c3881e260367242470e26f49ab
yang <642444906@qq.com>
2016-03-21 09:04:56 +0800

    change logger to log4j logger.

--- src/main/java/org/quartz/hello/HelloJob.java
+++ src/main/java/org/quartz/hello/HelloJob.java
@@ -1,15 +1,17 @@
 package org.quartz.hello;
 
+import org.apache.log4j.Logger;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
 public class HelloJob implements Job {
 
-    private static int count = 0;
+    private static int    count  = 0;
+    private static Logger logger = Logger.getLogger( HelloJob.class );
 
     public void execute( JobExecutionContext context ) throws JobExecutionException {
-        System.out.println( "Hello Job: " + count++ );
+        logger.info( "Hello Job: " + count++ );
     }
 
 }
